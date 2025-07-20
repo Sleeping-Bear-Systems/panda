53e333e6c116715397a3b4c76fa91e04a78ff682
Charles Farris <charlesfarris71@gmail.com>
2025-07-19 19:20:27 -0600

    Serve static content
    -Add web page
    -Add logo

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import { Hono } from "hono";
+import { serveStatic } from "hono/bun";
 
 const port = process.env.PORT ?? 3000;
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
+app.use("/*", serveStatic({ root: "./public" }));
 
 export default {
   port,
