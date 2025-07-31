89de3076072642ecb1b6322971d448509d3d991a
Charles Farris <charlesfarris71@gmail.com>
2025-07-30 23:40:43 -0600

    Replace bcrypt package

--- src/users/login.ts
+++ src/users/login.ts
@@ -35,7 +35,7 @@ export function mapLoginEndpoint(
     if (!user) {
       return c.text("Invalid username or password", 401);
     }
-    const isValidPassword = await bcrypt.compare(password, user.passwordHash);
+    const isValidPassword = await Bun.password.verify(password, user.passwordHash, "bcrypt");
     if (!isValidPassword) {
       return c.text("Invalid username or password", 401);
     }
@@ -63,19 +63,19 @@ function createTestUsers(): User[] {
     {
       id: "cc57350b-f778-4563-b214-0f9a1d5bc0d9",
       username: "admin",
-      passwordHash: bcrypt.hashSync("password1234", 10),
+      passwordHash: Bun.password.hashSync("password1234", {algorithm: "bcrypt", cost: 10}),
       role: "Administrator",
     },
     {
       id: "97dfe0c4-b01e-4fbf-a6df-9deb9287502c",
       username: "user",
-      passwordHash: bcrypt.hashSync("password1234", 10),
+      passwordHash: Bun.password.hashSync("password1234", {algorithm: "bcrypt", cost: 10}),
       role: "General",
     },
     {
       id: "44298c16-7e50-4966-abbc-f653679db543",
       username: "guest",
-      passwordHash: bcrypt.hashSync("password1234", 10),
+      passwordHash: Bun.password.hashSync("password1234", {algorithm: "bcrypt", cost: 10}),
       role: "Guest",
     },
   ];
