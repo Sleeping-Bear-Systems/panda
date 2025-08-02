3cbec58429fc8a0442237c8bccc02a4662ee38bd
Charles Farris <charlesfarris71@gmail.com>
2025-08-01 21:25:15 -0600

    Add components directory
    -Add <head> component

---
+++ src/components/head.tsx
@@ -0,0 +1,11 @@
+export function Head() {
+  return (
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Panda</title>
+      <link rel="stylesheet" href="/css/app.css" />
+      <script src="/js/htmx.min.js"></script>
+    </head>
+  );
+}
