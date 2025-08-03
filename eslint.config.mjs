4664fe6e1ee295f97099b98bca6a50664c6a8ad9
Charles Farris <charlesfarris71@gmail.com>
2025-08-03 15:31:39 -0600

    Configure "simple-import-sort"

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 
 import eslint from "@eslint/js";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
@@ -8,11 +9,19 @@ export default tseslint.config(
   tseslint.configs.strict,
   tseslint.configs.stylistic,
   {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+  },
+  {
     ignores: ["public/js/**"],
   },
   {
     rules: {
       "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+      "sort-imports": "off",
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
     },
   },
 );
