35e44499feae25b3600ffa43c0cd8e7d3ea81f5f
Charles Farris <charlesfarris71@gmail.com>
2025-08-14 22:23:16 -0600

    Remove redundant <body> tag

--- src/features/aboutPage.tsx
+++ src/features/aboutPage.tsx
@@ -6,27 +6,25 @@ import { ROUTES } from "../shared/routes.js";
 export const aboutPage = new Hono().get("/", (c) => {
   return c.html(
     <Layout title="About">
-      <body>
-        <img src="/images/sleeping_bear_logo.svg" alt="Sleeping Bear Logo" />
-        <nav>
-          <a href={ROUTES.HOME}>Home</a>
-        </nav>
-        <h1>About</h1>
-        <div>
-          Powered by&nbsp;
-          <a
-            href="https://event-driven-io.github.io/emmett/"
-            target="_blank"
-            rel="noopener"
-          >
-            Emmett
-          </a>
-        </div>
-        <br />
-        <div>
-          Copyright &copy; {new Date().getFullYear()} Sleeping Bear Systems
-        </div>
-      </body>
+      <img src="/images/sleeping_bear_logo.svg" alt="Sleeping Bear Logo" />
+      <nav>
+        <a href={ROUTES.HOME}>Home</a>
+      </nav>
+      <h1>About</h1>
+      <div>
+        Powered by&nbsp;
+        <a
+          href="https://event-driven-io.github.io/emmett/"
+          target="_blank"
+          rel="noopener"
+        >
+          Emmett
+        </a>
+      </div>
+      <br />
+      <div>
+        Copyright &copy; {new Date().getFullYear()} Sleeping Bear Systems
+      </div>
     </Layout>,
   );
 });
